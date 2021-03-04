9b7ac81269cd3b467bfddb8ace223f9f7a48a4f2
faitheden1 <faitheden54@gmail.com>
2021-03-04 13:22:59 -0800

    fixed code

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,35 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "What can you start a JavaScript statement with?",
+    choices: ["parentheses", "quotes", "semicolon", "colon"],
+    answer: "semicolon"
+  },
+  {
+    title: "What kind of file is JavaScript inserted to?",
+    choices: ["html", "css", "JavaScript", "arrays"],
+    answer: "html"
+  },
+  {
+    title: "What framework is used for designing a website?",
+    choices: [
+      "html",
+      "css",
+      "JavaScript",
+      "booleans"
+    ],
+    answer: "css"
+  },
+  {
+    title:
+      "What framework is the backbone for building a website?",
+    choices: ["JavaScript", "css", "numbers", "html"],
+    answer: "html"
+  },
+  {
+    title:
+      "Which symbol represents an ID?",
+    choices: ["&", " . ", "#", ";"],
+    answer: "#"
+  }
+];
